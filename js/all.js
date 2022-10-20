a821164625db9301192888c9e1742929d0e43f92
nora2207 <nora2207@outlook.com>
2022-10-20 11:45:06 +0800

    LightboxSwiper

---
+++ js/all.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    speed: 1000,
+    effect: 'slide',
+    autoplay: {
+      delay: 2000,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    
+  });
+
+    
+    
+
+
+
+});
+
+
+
